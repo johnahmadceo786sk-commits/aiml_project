25939bb03bd0a954e3fc6f9115114483ae6c341e
Ahmad <200~johnahmad.ceo.786.sk@gmail.com>
2026-02-10 14:36:58 +0530

    Refactor import statements to use relative paths

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from model import train_model, load_model
-from database import SessionLocal, Prediction
-
+from .model import train_model, load_model
+from .database import SessionLocal, Prediction
 
 app = FastAPI()
 
-
+# Train/load model once
 train_model()
 model = load_model()
 
-
 class Input(BaseModel):
     hours: float
-
 
 @app.post("/predict")
 def predict(data: Input):
     prediction = model.predict([[data.hours]])[0]
 
-
     db = SessionLocal()
     record = Prediction(hours=data.hours, predicted_marks=prediction)
     db.add(record)
     db.commit()
-
 
     return {"predicted_marks": round(prediction, 2)}
